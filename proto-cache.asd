d40f372f0942026d0c46d43275828771207eba3e
jgodbou <jgodbou@penguin>
2021-01-24 00:03:49 -0500

    Proto-izing proto-cache

--- proto-cache.asd
+++ proto-cache.asd
@@ -4,9 +4,14 @@
   :licence "MIT-style"
   :description      "A simple cache utility for protos."
   :long-description "A simple cache utility for protos."
+  :defsystem-depends-on (:cl-protobufs)
   :depends-on (:ace.core :cl-protobufs :drakma)
   :components
   ((:module "src"
     :serial t
     :pathname ""
-    :components ((:file "proto-cache")))))
+    :components
+    ((:protobuf-source-file "pub-sub-details"
+      :proto-pathname "pub-sub-details.proto"
+      :proto-search-path ("../cl-protobufs/google/protobuf/"))
+     (:file "proto-cache")))))
--- proto-cache.lisp
+++ proto-cache.lisp
@@ -2,50 +2,39 @@
   (:use #:cl)
   (:export #:register-publisher
            #:register-subscriber
-           #:update-publisher-any)
+           #:update-publisher-any
+           #:save-state-to-file
+           #:load-state-from-file)
   (:local-nicknames
    (#:act #:ace.core.thread)
    (#:acd #:ace.core.defun)
    (#:ace #:ace.core.etc)
-   (#:google #:cl-protobufs.google.protobuf)))
+   (#:google #:cl-protobufs.google.protobuf)
+   (#:psd #:cl-protobufs.pub-sub-details)))
 
 (in-package #:proto-cache)
 
-(defvar *cache* (make-hash-table :test 'equal))
+(defvar *cache* (psd:make-pub-sub-details-cache))
+(defvar *mutex-for-pub-sub-details* (make-hash-table :test 'equal))
 (defvar *cache-mutex* (act:make-frmutex))
+;; (cl-protobufs.implementation::make-serializer psd:pub-sub-details-cache)
 
-;; This pub-sub-details class stuff should be in it's
-;; own file.
-(defclass pub-sub-details ()
-  ((subscriber-list :type list
-                    :initform nil
-                    :reader subscriber-list)
-   (current-any :type google:any
-                :initform (google:make-any)
-                :reader current-any)
-   (mutex :type act:frmutex
-          :initform (act:make-frmutex)
-          :reader mutex
-          :documentation "A mutex to protext the current-any slot.")
-   (password :type string
-             :reader password
-             :initarg :password
-             :documentation "The worst way to store a password ever...")))
-
-(acd:defun* make-pub-sub-details (password)
+(acd:defun* make-pub-sub-details (username password)
   "Make the pub-sub-details struct with a given password."
-  (declare (acd:self (string) pub-sub-details))
-  (make-instance 'pub-sub-details :password password))
+  (declare (acd:self (string string) psd:pub-sub-details))
+  (psd:make-pub-sub-details :username username
+                            :password password
+                            :current-any (google:make-any)))
 
-(defmethod (setf current-any) (new-value (psd pub-sub-details))
+(defmethod (setf psd:current-any) :around (new-value (psd psd:pub-sub-details))
   "Set the new current-any field on a `pub-sub-details` class.
    Send the new google:any message to any subscriber."
-  (let ((ps-mutex (mutex psd)))
+  (let ((ps-mutex (gethash (psd:username psd) *mutex-for-pub-sub-details*)))
     (act:with-frmutex-write (ps-mutex)
-      (setf (slot-value psd 'current-any) new-value))))
+      (call-next-method))))
 
-(defmethod (setf current-any) :after (new-value (psd pub-sub-details))
-  (let ((subscriber-list (subscriber-list psd)))
+(defmethod (setf psd:current-any) :after (new-value (psd psd:pub-sub-details))
+  (let ((subscriber-list (psd:subscriber-list psd)))
     (dolist (subscriber subscriber-list)
       (unwind-protect
            (drakma:http-request
@@ -54,31 +43,36 @@
             :content (cl-protobufs:serialize-to-bytes new-value))
         (print subscriber)))))
 
-(defmethod (setf subscriber-list) (new-value (psd pub-sub-details))
+(defmethod (setf psd:subscriber-list) :around (new-value (psd psd:pub-sub-details))
   "Setf function for the subscriber list.
    Locks the pub-sub-details frmutex before writing."
-  (let ((ps-mutex (mutex psd)))
+  (let ((ps-mutex (gethash (psd:username psd) *mutex-for-pub-sub-details*)))
     (act:with-frmutex-write (ps-mutex)
-      (setf (slot-value psd 'subscriber-list) new-value))))
+      (call-next-method))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; The real pub-sub-functions.
 
 (defun register-publisher (username password)
   "Register a publisher with the proto-cache."
-  (let ((pub-sub-struct (make-pub-sub-details password)))
+  (let ((pub-sub-struct (make-pub-sub-details username password)))
     (act:with-frmutex-write (*cache-mutex*)
-      (unless (gethash username *cache*)
-        (setf (gethash username *cache*) pub-sub-struct)))))
+      (unless (psd:pub-sub-cache-gethash username *cache*)
+        (setf (psd:pub-sub-cache-gethash username *cache*)
+              pub-sub-struct
+              (gethash username *mutex-for-pub-sub-details*)
+              (act:make-frmutex))
+        t))))
 
 (defun register-subscriber (publisher address)
   "Register a new subscriber to a publisher."
   (ace:clet ((ps-struct
               (act:with-frmutex-read (*cache-mutex*)
-                (gethash publisher *cache*)))
-             (ps-mutex (mutex ps-struct)))
+                (psd:pub-sub-cache-gethash publisher *cache*)))
+             (ps-mutex
+              (gethash publisher *mutex-for-pub-sub-details*)))
     (act:with-frmutex-write (ps-mutex)
-      (push address (subscriber-list ps-struct)))))
+      (push address (psd:subscriber-list ps-struct)))))
 
 (defun update-publisher-any (username password any)
   "Updates the google:any message for a publisher
@@ -88,12 +82,38 @@
    was truly updated."
   (ace:clet ((ps-class
               (act:with-frmutex-read (*cache-mutex*)
-                (gethash username *cache*)))
-             (correct-password (string= (password ps-class)
+                (psd:pub-sub-cache-gethash username *cache*)))
+             (correct-password (string= (psd:password ps-class)
                                         password)))
     (declare (ignore correct-password))
     (act:make-thread
      (lambda (ps-class)
-       (setf (current-any ps-class) any))
+       (setf (psd:current-any ps-class) any))
      :arguments (list ps-class))
     t))
+
+(defun save-state-to-file (&key (filename "/tmp/proto-cache.txt"))
+  "Save the current state of the proto cache to *cache* global
+   to FILENAME as a serialized protocol buffer message."
+  (act:with-frmutex-read (*cache-mutex*)
+    (with-open-file (stream filename :direction :output
+                                     :element-type '(unsigned-byte 8))
+      (cl-protobufs:serialize-to-stream stream *cache*))))
+
+(defun load-state-from-file (&key (filename "/tmp/proto-cache.txt"))
+  "Load the saved *cache* globals from FILENAME. Also creates
+   all of the fr-mutexes that should be in *mutex-for-pub-sub-details*."
+  (let ((new-cache
+          (with-open-file (stream filename :element-type '(unsigned-byte 8))
+            (cl-protobufs:deserialize-from-stream
+             'psd:pub-sub-details-cache :stream stream)))
+        (new-mutex-for-pub-sub-details (make-hash-table :test 'equal)))
+    (loop for key being the hash-keys of (psd:pub-sub-cache new-cache)
+          do
+             (print key)
+             (print (psd:pub-sub-cache-gethash key new-cache))
+             (setf (gethash key new-mutex-for-pub-sub-details)
+                   (act:make-frmutex)))
+    (act:with-frmutex-write (*cache-mutex*)
+      (setf *mutex-for-pub-sub-details* new-mutex-for-pub-sub-details
+            *cache* new-cache))))
